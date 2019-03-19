b3ca2a5a325c3c91a47fe22d0340e80f49a767dc
Heling Zhu <heling.zhu@cern.ch>
2019-03-19 21:19:07 +0100

    add one setup in build

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ mkdir build
 cd build
 rm -rf * 
 acmSetup --sourcedir=../source AnalysisBase,21.2.26
+cp ../setup_bld.sh .
 #asetup AnalysisBase,21.2.26
 #source /afs/cern.ch/work/h/hezhu/public/workplace/DarkPh/code4l_dp/build/x86_64-slc7-gcc62-opt/setup.sh
 
---
+++ setup_bld.sh
@@ -0,0 +1,7 @@
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+#setupATLAS
+asetup AnalysisBase,21.2.26
+# Will auto make
+make
+#source /afs/cern.ch/work/h/hezhu/public/workplace/DarkPh/code4l_dp/build/x86_64-slc7-gcc62-opt/setup.sh
